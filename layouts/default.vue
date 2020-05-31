132d0fb6fc0dee01d5c8e62f7c4b0587f15f99d5
Satoshi Ota <sota.developer@gmail.com>
2020-05-31 10:03:28 +0900

    Add register

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header />
+    <Alert />
     <nuxt />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header'
+import Alert from '~/components/Alert'
 
 export default {
   components: {
-    Header
+    Header,
+    Alert
   }
 }
 </script>
